3bfef976c15f65b506f20ed3d942d093fb7a8adc
Miguel Oppermann <miguel.o@gmx.net>
2026-01-01 12:57:49 +0100

    Update interface.cpp

--- boards/ESP-General/interface.cpp
+++ boards/ESP-General/interface.cpp
@@ -1,49 +1,56 @@
 #include "core/powerSave.h"
+#include "core/utils.h"
 #include <interface.h>
 
 /***************************************************************************************
-** Function name: _setup_gpio()
-** Location: main.cpp
-** Description:   initial setup for the device
+** GPIO setup
 ***************************************************************************************/
-void _setup_gpio() { bruceConfig.startupApp = "WebUI"; }
+void _setup_gpio() {
+
+    pinMode(TFT_CS, OUTPUT);
+    digitalWrite(TFT_CS, HIGH);
+
+    pinMode(TFT_MOSI, OUTPUT);
+    digitalWrite(TFT_MOSI, HIGH);
+
+    pinMode(TFT_SCLK, OUTPUT);
+
+    pinMode(TFT_DC, OUTPUT);
+    digitalWrite(TFT_DC, HIGH);
+
+    pinMode(TFT_RST, OUTPUT);
+
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+
+#ifdef ILI9341_DRIVER
+    bruceConfig.colorInverted = 0;
+#endif
+}
 
 /***************************************************************************************
-** Function name: getBattery()
-** location: display.cpp
-** Description:   Delivers the battery value from 1-100
+** Battery / charging
 ***************************************************************************************/
 int getBattery() { return 0; }
-
-/***************************************************************************************
-** Function name: isCharging()
-** Description:   Default implementation that returns false
-***************************************************************************************/
 bool isCharging() { return false; }
 
-/*********************************************************************
-** Function: setBrightness
-** location: settings.cpp
-** set brightness value
-**********************************************************************/
-void _setBrightness(uint8_t brightval) {}
+/***************************************************************************************
+** Brightness
+***************************************************************************************/
+void _setBrightness(uint8_t brightval) {
+    if (brightval == 0) {
+        analogWrite(TFT_BL, 0);
+    } else {
+        int bl = MINBRIGHT + round(((255 - MINBRIGHT) * brightval / 100));
+        analogWrite(TFT_BL, bl);
+    }
+}
 
-/*********************************************************************
-** Function: InputHandler
-** Handles the variables PrevPress, NextPress, SelPress, AnyKeyPress and EscPress
-**********************************************************************/
+/***************************************************************************************
+** Input (no buttons, no touch for now)
+***************************************************************************************/
 void InputHandler(void) {}
 
-/*********************************************************************
-** Function: powerOff
-** location: mykeyboard.cpp
-** Turns off the device (or try to)
-**********************************************************************/
+/***************************************************************************************/
 void powerOff() {}
-
-/*********************************************************************
-** Function: checkReboot
-** location: mykeyboard.cpp
-** Btn logic to turnoff the device (name is odd btw)
-**********************************************************************/
 void checkReboot() {}
